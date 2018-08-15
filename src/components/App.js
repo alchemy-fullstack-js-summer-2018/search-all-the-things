dbe8f1b245cb2773c5ef906d4141c3595ae2b29d
MariahAdams <mariah.adams23@yahoo.com>
2018-08-15 16:52:21 -0700

    hooked up App.css to component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import styles from './App.css'
 
 class App extends Component {
 
   render() {
 
     return (
-      <main>
+      <main className={styles.app}>
         <h1>Hello World</h1>
       </main>
     )
