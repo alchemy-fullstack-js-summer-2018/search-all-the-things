cc9082f0411558bbc82ba056b6b5f42d16034163
MariahAdams <mariah.adams23@yahoo.com>
2018-08-15 16:33:54 -0700

    more file setup: adding in jest and enzyme config files, hooking up app component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    )
+  }
+}
+
+export default App;
