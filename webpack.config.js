5bfe11996cc77b871cf3d0fb0c8ea7d37b968495
MariahAdams <mariah.adams23@yahoo.com>
2018-08-15 16:17:29 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+/* eslint-env node */
+const { resolve } = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+
+const buildDir = 'docs';
+const path = resolve(__dirname, buildDir);
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path, 
+    filename: 'bundle.[hash].js',
+    publicPath: ''
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: `./${buildDir}`
+  },
+  plugins: [
+    new CleanPlugin(`${path}/bundle.*.js`),
+    new HtmlPlugin({ template: './src/index.html' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', {
+                targets: {
+                  browsers: 'Chrome 65'
+                }
+              }],
+              'react'
+            ],
+            plugins: [
+              require('babel-plugin-transform-object-rest-spread'),
+              require('babel-plugin-transform-class-properties')
+            ],
+            cacheDirectory: true
+          }
+        }
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true }
+          }
+        ]
+      },
+
+      {
+        test: /\.(jpg|png|svg|jpeg)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 1000 }
+        }
+      }
+
+    ]
+  }
+};
