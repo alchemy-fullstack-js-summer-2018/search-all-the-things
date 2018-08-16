dcab44554422346a61bd10f8e2ec128d48b4c443
Mario Quintana <pyrros2000@gmail.com>
2018-08-16 08:00:59 -0700

    Created search component will create app next

---
+++ src/components/search/Search.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import './Search.css';
+
+export default class Search extends Component {
+
+    state = {
+        search: ''
+    };
+
+    render() {
+        const { search } = this.state
+
+        return (
+            <form className="search-form" onSubmit={event => this.handleSubmit(event)}>
+            <label>
+                Search For:&nbsp;
+                <input value={search} onChange={this.handleChange}/>
+            </label>
+            <button>Search</button>
+            </form>
+        );
+    }
+}
