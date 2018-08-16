0f42408c51cb003bb89ea75b83ccf552f785de24
Mark Myers <markalope@gmail.com>
2018-08-16 11:18:28 -0700

    deleted LAstfm API info until I move to .env

--- src/services/lastFmApi.js
+++
@@ -1,7 +0,0 @@
-const API_KEY = '9e217382d31d48af8c3bc200cdc6ddbd';
-const API_QUERY = `apiKey=${API_KEY}`;
-const BASE_URL = 'http://ws.audioscrobbler.com/2.0/';
-const EVERYTHING_URL = `${BASE_URL}/everything?${API_QUERY}`;
-const SOURCES_URL = `${BASE_URL}/sources?${API_QUERY}`;
-const SORT_QUERY = 'sortBy=publishedAt';
-
