5a60f3d9e3a48f3b2ca07e45d14235e9772e9958
rbtprograms <rbt61690@gmail.com>
2018-08-16 10:00:05 -0700

    rendering header

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class App extends Component {
+
+  render() {
+    
+    return (
+      <div>
+        <header>
+          <Header/>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  render() {
+    return ( 
+      <div>
+        <section>
+          <h1>Bobbys Book Emporium</h1>
+          <h4>Hey, you need some books? Well thats the only thing we have!</h4>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Header;
