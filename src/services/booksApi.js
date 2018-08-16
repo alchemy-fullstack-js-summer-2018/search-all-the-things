a3bd1e8721c6b3cea6b246f17e2f3e17f2c43981
MariahAdams <mariah.adams23@yahoo.com>
2018-08-16 09:41:41 -0700

    working on booksApi file

--- src/services/booksApi.js
+++ src/services/booksApi.js
@@ -1,18 +1,22 @@
-//TODO: sort out env variables (no api key needed for google books)
-// const API_QUERY = '';?
-// everything, sources, sort query?
-
-const BASE_URL = 'https://www.googleapis.com/books/v1'; //search by volumes
+//TODO: env?
+const BASE_URL = 'https://www.googleapis.com/books/v1';
+const VOLUMES_URL = `${BASE_URL}/volumes?`;
+const BOOK_URL = `${BASE_URL}/volumes`;
 
 const throwJson = json => { throw json; };
 const get = url => fetch(url)
   .then(r => r.ok ? r.json() : r.json().then(throwJson));
 
-export function search({ /* TODO */ topic, sources = ['bad'] }, { page = 1, pageSize = 20 }) {
-  const search = `&q=${topic}&sources=${sources.join()}`;
-  const paging = `&page=${page}&pageSize=${pageSize}`;
+/* TODO: test file? */
+export function search({ term, page, perPage }) {
+  const bookIndex = (page * perPage) - perPage;
+  const search = `&q=${term}`;
+  const paging = `&maxResults=${perPage}&startIndex=${bookIndex}`;
 
-  return get(`${EVERTHING_URL}${search}${paging}${sort}`);
+  return get(`${VOLUMES_URL}${search}${paging}`);
 }
 
-//get sources fn?
+export function getBook(volumeId) {
+  return get(`${BOOK_URL}/${volumeId}`)
+    .then(r => r.books);
+}
