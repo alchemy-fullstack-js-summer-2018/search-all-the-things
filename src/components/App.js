731f19ee2b8fa9ca4286ea9e1a845566be7f0a22
carriecarter <carrie@carrie-carter.com>
2018-08-16 11:34:22 -0700

    basic html rendering to DOM

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import styles from './App.css';
+
+class App extends Component {
+
+  handleSearch = () => {
+
+  };
+
+  render() {
+
+    return (
+      <div className={styles.app}>
+        <header>
+          <h1>Search All The Things</h1>
+          
+        </header>
+        <main>
+
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
