8e73582113bcea074ff853351dbb8fe572f08786
Rehmsy <sarahrehmer@gmail.com>
2018-08-16 12:02:17 -0700

    last commit before due

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,3 @@
-// import React, { Component } from 'react';
-// //import styles from './App.css';
-
-// class App extends Component {
-//   state = {
-//     content: 'hi ya dummy',
-//     url: 'https://atlantisbahamas.com/media/Things%20To%20Do/Water%20Park/Beaches/Widget/Beaches_CoveBeach.jpg'
-//   };
-
-//   render() {
-//     //const { content } = this.state;
-
-//     return (
-//       <main>
-//         <section>
-//           <h2>is i showing??</h2>
-
-//         </section>
-//       </main>
-//     );
-//   }
-// }
-
-// export default App;
-
-
 import React, { Component } from 'react';
 //import Header from './Header';
 //import styles from './App.css';
