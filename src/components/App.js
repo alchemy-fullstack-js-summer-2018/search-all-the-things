70df1dc7fe6224d4598ab18279a27b3bc0a9c2ed
Mario Quintana <pyrros2000@gmail.com>
2018-08-16 08:07:26 -0700

    Three core components all render on webpage. Will need to start testing for rendering other parts and function

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import styles from './App.css';
+
+class App extends Component {
+
+    handleSearch = () => {
+
+    };
+
+    render() {
+        return (
+            <div className={styles.app}>
+                <header>
+                    <Header onSearch={this.handleSearch}/>
+                </header>
+                <main>
+
+                </main>
+
+            </div>
+        );
+    }
+}
+
+export default App;
