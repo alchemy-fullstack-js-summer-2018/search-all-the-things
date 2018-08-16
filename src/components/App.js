d856037546841fd2dbb106136b5fb50e3dd1907a
Robyn Navarro <rnavarro182@gmail.com>
2018-08-15 20:32:18 -0700

    react rendering

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styles from './App.css';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className={styles.app}>
+        <header>
+          It is alive!
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
