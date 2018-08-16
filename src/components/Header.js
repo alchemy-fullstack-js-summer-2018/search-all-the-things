c90cecb71ab8f08651307b41fa5a9de2b8e48e67
Mark Myers <markalope@gmail.com>
2018-08-16 09:23:54 -0700

    built out Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,3 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Search from './search/Search';
 import logo from '../assets/logo.png';
+import styles from './Header.css';
+
+class Header extends Component {
+
+  static propTypes = {
+    onSearch: PropTypes.func.isRequired
+  };
+
+  render() {
+    const { onSearch } = this.props;
+
+    return (
+      <div className={styles.header}>
+        <section className="header-container">
+          <img src={logo}/>
+          <h1>News Search</h1>
+        </section>
+
+        <section className="search-container">
+          <Search onSearch={onSearch}/>
+        </section>
+      </div>
+    );
+  }
+}
+export default Header;
