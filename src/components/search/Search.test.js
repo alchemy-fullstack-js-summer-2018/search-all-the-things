a3e9ba1ff13034fc10eda1c35383c9739897001c
Mario Quintana <pyrros2000@gmail.com>
2018-08-16 10:15:04 -0700

    Search criteria test now passing

--- src/components/search/Search.test.js
+++ src/components/search/Search.test.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount  } from 'enzyme';
 import Search from './Search';
 import toJSON from 'enzyme-to-json';
 
 
 describe('Search', () => {
     
-  it('renders as designed', () => {
+  it('Renders as designed', () => {
     const wrapper = shallow(<Search onSearch={() => {}}/>);
     expect(toJSON(wrapper)).toMatchSnapshot();
+  });
+
+  it('Calls onSearch with criteria entered', () => {
+    const handleSearch = jest.fn();
+    const wrapper = mount(<Search onSearch={handleSearch}/>);
+
+    const search = 'Skyline';
+
+    wrapper.find('input').simulate('change', {
+      target: { value: search }
+    });
+    wrapper.find('button').simulate('submit');
+
+    const calls = handleSearch.mock.calls;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toEqual({ search });
   });
 });
