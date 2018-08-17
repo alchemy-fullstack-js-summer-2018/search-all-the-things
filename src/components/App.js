b769d4bdfd6c75475101aa72c5e83b2b3e2d120f
carriecarter <carrie@carrie-carter.com>
2018-08-16 18:31:49 -0700

    header setup in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-//import Header from './Header';
-//import styles from './App.css';
+import Header from './Header';
+import styles from './App.css';
 
 class App extends Component {
 
@@ -11,7 +11,10 @@ class App extends Component {
   render() {
     
     return (
-      <div>
+      <div className={styles.app}>
+        <header>
+          <Header onSearch={this.handleSearch}/>
+        </header>
       
         <main>
           <h2>do u see me? please see me yo.</h2>
