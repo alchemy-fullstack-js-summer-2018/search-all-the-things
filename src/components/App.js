5422ab014c9c7446dc27bfde31f41a64d74b845d
MariahAdams <mariah.adams23@yahoo.com>
2018-08-17 15:24:50 -0700

    paging started, fixed syntax

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Header from './Header';
 import Books from './books/Books';
 import Paging from './paging/Paging';
@@ -8,18 +8,29 @@ import { search } from '../services/booksApi';
 class App extends Component {
 
   state = {
-    data: []
+    data: null,
+    search: null,
+    page: 1,
+    perPage: 20,
+    totalResults: 0
   };
 
   handleSearch = (term) => {
+    this.setState({ search: term.search });
     console.log(`You searched for ${term.search}`);
-
     search(term)
       .then(results => this.setState({ data: results }));
     console.log('****AFTER SEARCH*****');
   };
 
+  handlePage = paging => {
+    this.setState(paging, () => {
+      this.search();
+    });
+  };
+
   render() {
+    const { data, search, page, perPage, totalResults } = this.state;
 
     return (
       <main className={styles.app}>
@@ -29,11 +40,24 @@ class App extends Component {
         </header>
 
         <section>
-          <Paging />
-        </section>
-
-        <section>
-          <Books books={this.state.data.items}/>
+          {data &&
+          <Fragment>
+            <p>
+              Searching for &quot;{search}&quot;
+            </p>
+            <Paging 
+              page={page}
+              perPage={perPage}
+              totalResults={totalResults}
+              onPage={this.handlePage}
+            />
+          </Fragment>  
+          }
+        
+          {data
+            ? <Books books={data.items}/>
+            : <p>Please enter a search to get started</p>
+          }
         </section>
       </main>
     );
