3288d8a146e89028b8c2d04f75c5661e022a404b
Mario Quintana <pyrros2000@gmail.com>
2018-08-17 15:15:34 -0700

    Paging component complete will connect to new results component

--- src/components/paging/Paging.js
+++ src/components/paging/Paging.js
@@ -4,14 +4,24 @@ import PropTypes from 'prop-types';
 export default class Paging extends Component {
 
     static propTypes = {
-
+      page: PropTypes.number,
+      perPage: PropTypes.number,
+      totalResults: PropTypes.number,
+      onPage: PropTypes.func.isRequired
     };
 
-    handlePage() {
+    handlePage(increment) {
+      const { page, onPage } = this.props;
+      onPage({ page: page + increment });
 
     }
 
     render() {
+      const { totalResults, page, perPage } = this.props;
+
+      if(!totalResults) return <div> No Results Foudn, try another search</div>;
+
+      const totalPages = Math.ceil(totalResults / perPage);
       return (
         <div>
           <span>Page {page} of {totalPages}</span>
