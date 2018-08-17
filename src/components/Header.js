d8412d4a075c94a004c2b1522cc5fc84b5a75418
Antreo Pukay <acrosswalker@yahoo.com>
2018-08-17 15:55:18 -0700

    header.js complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Search from './search/Search';
+import { Link, Route } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import styles from './Header.css';
 
 class Header extends Component {
 
-  static propTypes = {
-    onSearch: PropTypes.func.isRequired
-  };
-
   render() {
-    const { onSearch } = this.props;
 
     return (
-      <div className={styles.header}>
+      <div className={styles.header}>  
         <section className="header-container">
-          <img src={logo}/>
-          <h1>News Search</h1>
-        </section>
-
-        <section className="search-container">
-          <Search onSearch={onSearch}/>
-        </section>
-      </div>
+          <div className="logo"><img src={logo}/>
+            <h1>Album Search</h1>
+          </div> 
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/favorites">Favorites</Link>
+              </li>
+              <li>
+                <Link to="/albums">Search Albums</Link>
+              </li>
+            </ul>
+          </nav>
+        </section> 
+          <section className="search-container">
+            <Search onSearch={onSearch}/>
+          </section>
+        </div>
     );
   }
 }
