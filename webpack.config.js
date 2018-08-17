612b52b1ec8dbc1db9858546dd1f0200683e4194
Rehmsy <sarahrehmer@gmail.com>
2018-08-16 17:51:09 -0700

    all basic setup complete

--- webpack.config.js
+++ webpack.config.js
@@ -7,82 +7,69 @@ const buildDir = 'docs';
 const path = resolve(__dirname, buildDir);
 
 module.exports = {
-    // start here
-    entry: './src/index.js',
-    // put the build output here (not dev server)
-    output: {
-        path,
-        filename: 'bundle.[hash].js',
-        publicPath: ''
-    },
-    // mode (will eventually be cmd line arg in package.json scripts)
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './docs',
-    },
-    plugins: [
+  // start here
+  entry: './src/index.js',
+  // put the build output here (not dev server)
+  output: {
+    path,
+    filename: 'bundle.[hash].js',
+    publicPath: ''
+  },
+  // mode (will eventually be cmd line arg in package.json scripts)
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: `./${buildDir}`,
+  },
+  plugins: [
+    // add plugins
+    new CleanPlugin(`${path}/bundle.*.js`),
+    new HtmlPlugin({ template: './src/index.html' })
+  ],
+  module: {
+    rules: [
+      // js
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
 
-        new CleanPlugin(`${path}/bundle.*.js`),
-        new HtmlPlugin({ template: './src/index.html' })
-    ],
-    module: {
-        rules: [
-            // js
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ['env', {
-                                targets: {
-                                    browsers: ['last 2 versions', 'safari >= 7']
-                                },
-                            }],
-                            'react'
-                        ],
-                        plugins: [
-                            require('babel-plugin-transform-object-rest-spread'),
-                            require('babel-plugin-transform-class-properties')
-                        ],
-                        cacheDirectory: true
-                    }
-                }
-
-            },
-
-            // css
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                        options: { sourceMap: true }
-                    },
-            {
-                loader: 'css-loader',
-                options: {
-                    sourceMap: true,
-                    importLoaders: 1
-                }
-            },
-            {
-                loader: 'postcss-loader',
-                options: { sourceMap: true }
+      // css
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { sourceMap: true }
+          },
+          {
+            loader: 'css-loader',
+            options: { 
+              sourceMap: true,
+              importLoaders: 1 
             }
-                ]
-            },
-
-            // images
-            // {
-            //     test: /\.(jpg|png|svg)$/,
-            //     use: {
-            //         loader: 'url-loader',
-            //         options: { limit: 1000 },
-            //     },
-            // }
+          },
+          {
+            loader: 'postcss-loader',
+            options: { sourceMap: true }
+          }
         ]
-    }
+      },
+
+      // images
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 1000 },
+        },
+      }
+    ]
+  }
 };
