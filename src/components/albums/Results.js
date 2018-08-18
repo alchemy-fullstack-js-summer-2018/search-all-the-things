3368de0c2cf6edc66e667099ddab4a5585db6d3f
Mark Myers <markalope@gmail.com>
2018-08-18 14:24:22 -0700

    building out Results.js

--- src/components/albums/Results.js
+++ src/components/albums/Results.js
@@ -5,3 +5,99 @@ import Albums from './Albums';
 import Paging from '../paging/Paging';
 import { getAlbums } from '../../services/lastFmApi';
 
+class Results extends Component {
+
+  state = {
+    albums: null,
+    totalResults: 0,
+    page: 1,
+    perPage: 10,
+    loading: false,
+    error: null
+  };
+
+  static PropTypes = {
+    location: PropTypes.object.isRequired
+  };
+
+  componentDidMount() {
+    this.searchAlbums();
+  }
+
+  componentDidUpdate({ location }) {
+    const { search: oldSearch } = qs.parse(location.search);
+    if(oldSearch === this.searchTerm) return;
+    this.searchAlbums();
+  }
+
+  handlePage = paging => {
+    this.setState(paging, () => {
+      this.searchAlbums();
+    });
+  };
+
+  get searchTerm() {
+    const { location } = this.props;
+    const { search } = qs.parse(location.search);
+    return search;
+  }
+
+  searchAlbums() {
+
+    this.setState({
+      loading: true,
+      error: null
+    });
+
+    getAlbums()
+      .then(
+        ({ results }) => {
+          this.setState({
+            albums: results
+          });
+        },
+        err => {
+          this.setState({ error: err.message });
+        }
+      )
+      .then(() => {
+        this.setState({ loading: false });
+      });
+  }
+
+  render () {
+    const { albums, loading, error } = this.state;
+    const { page, perPage, totalResults } = this.state;
+    const { searchTerm } = this;
+
+    return (
+      <section>
+        {(loading || error) &&
+          <section className="notifications">
+            {loading && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+          </section>
+        }
+
+        {searchTerm &&
+          <Fragment>
+            <p>Searching for &quot;{searchTerm}&quot;</p>
+            <Paging
+              page={page}
+              perPage={perPage}
+              totalResults={totalResults}
+              onPage={this.handlePage}
+            />
+          </Fragment>
+        }
+
+        {albums
+          ? <Albums albums={albums}/>
+          : <p>Please enter a search to get started</p>
+        }
+      </section>   
+    );
+  }
+}
+
+export default Results;
