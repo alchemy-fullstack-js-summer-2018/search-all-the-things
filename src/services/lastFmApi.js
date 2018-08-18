2d9abc2cee6234a8e4e048259b9a618579c575e1
Mark Myers <markalope@gmail.com>
2018-08-18 13:05:50 -0700

    details search is working.

--- src/services/lastFmApi.js
+++ src/services/lastFmApi.js
@@ -1,3 +1,4 @@
-const API_KEY = '';
-const API_QUERY = `apiKey=${API_KEY}`;
+const API_KEY = '9ff12e67d0cd5b5a51730b280660b0fc';
+const API_QUERY = `method=album.search&album=moon&apiKey=${API_KEY}&format=json`;
 const BASE_URL = 'http://ws.audioscrobbler.com/2.0/';
+cosnt EVERYTHING_URL = `${BASE_URL}/?${API_QUERY}`;
