91cdb9d45acd4820dc15ba1730e8ff5acc33ecf9
MariahAdams <mariah.adams23@yahoo.com>
2018-08-20 10:08:31 -0700

    added favorite and favorites components with their scaffolding

---
+++ src/components/favorites/Favorite.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { getFavorite } from '../../services/favoritesApi';
+
+class Favorite extends Component {
+
+  render() {
+
+    return (
+      <li>
+
+      </li>
+    );
+  }
+}
+
+export default Favorite;
---
+++ src/components/favorites/Favorites.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { getFavorites } from '../../services/favoritesApi';
+import Favorite from './Favorite';
+
+class Favorites extends Component {
+
+  render() {
+
+    return (
+      <ul>
+
+      </ul>
+    );
+  }
+}
+
+export default Favorites;
