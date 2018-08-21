9b3167abd05f0e27a8a05e2ce4efafa8d3bbd203
MariahAdams <mariah.adams23@yahoo.com>
2018-08-21 11:20:38 -0700

    updated jest config file

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -1,7 +1,12 @@
 /* eslint-env node */
 module.exports = {
+  roots: ['..'],
   setupTestFrameworkScriptFile: '<rootDir>/enzyme.setup.js',
   transform: {
-    '.*': '<rootDir>/node_modules/jest-css-modules'
-  }
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/fileMock.js',
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+  },
 };
